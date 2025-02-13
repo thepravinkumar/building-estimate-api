6a73408dcb1ac7ecfabc7e8f603ec645ccc254be
thepravinkumar <pravinkumar261003@gmail.com>
2025-02-14 02:52:04 +0530

    push it

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+CORS(app, origins="*")  # Allow all origins
 
-# Sample CPWD rate per cubic meter (modify as per latest CPWD rates)
 CPWD_RATE_PER_CUBIC_METER = 25000
 
 @app.route('/estimate', methods=['POST'])
@@ -11,13 +12,13 @@ def estimate():
     base_area = data.get('baseArea')
     floor_height = data.get('floorHeight')
     total_floors = data.get('totalFloors')
-    
+
     if not base_area or not floor_height or not total_floors:
         return jsonify({'error': 'Missing required parameters'}), 400
-    
+
     total_volume = base_area * floor_height * total_floors
     estimated_cost = total_volume * CPWD_RATE_PER_CUBIC_METER
-    
+
     return jsonify({'estimatedCost': estimated_cost})
 
 if __name__ == '__main__':
